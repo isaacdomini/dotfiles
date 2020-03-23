d148af15f80c5755d3afb87cdb259d3b3991745c
Isaac Domini <me@isaacdomini.com>
2020-03-23 00:57:14 -0400

    Update fullsetup.sh

--- ubuntu/fullsetup.sh
+++ ubuntu/fullsetup.sh
@@ -20,14 +20,10 @@ echo "Installing zsh"
 sudo apt-get install zsh
 echo "Setting  zsh as default"
 chsh -s $(which zsh)
-echo "installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "Changing theme to bureau"
-sed 's/.*ZSH_THEME.*/ZSH_THEME="bureau"/' ~/.zshrc
-echo "INSTALLING NERD TREE"
-curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh 
+
+echo "source ~/dotfiles/defaults" >> ~/.zshrc
+
 source ~/.zshrc
-apt-vim install -y https://github.com/scrooloose/nerdtree.git
-sudo sh -c 'echo "autocm vimenter * NERDTree" >> ~/.vimrc'
+
 echo "\n\n...Essentials Installed...\n\n"
 
